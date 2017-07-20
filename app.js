c7bb5af8eefb5d9dd6fb9e9bbc42aa7d3cb86855
hassenfath <fathallah.houssem@gmail.com>
2017-07-20 07:17:30 +0100

    add footer template

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-LRU = require('lru-cache');
+"use strict";
+let express = require('express');
+let path = require('path');
+let favicon = require('serve-favicon');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+let LRU = require('lru-cache');
 
-var app = express();
-var i18n = require("i18n");
+
+let app = express();
+let i18n = require("i18n");
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 
 i18n.configure({
     locales:['ar', 'en', 'fr'],
@@ -14,9 +24,33 @@ i18n.configure({
     // sets a custom cookie name to parse locale settings from - defaults to NULL
     cookie: 'LOCALE',
 });
+
+//app.use(bodyParser);
+//app.use(cookieParser);
+
 app.use(i18n.init);
+
+app.post('/locale', (req, res) => {
+    i18n.setLocale(req, req.body.locale);
+    req.session.locale = req.body.locale;
+    console.log(req.body.locale);
+    res.redirect('/')
+});
+
+// set active url
+app.use(function(req, res, next){
+    let paths = req.path.split('/') ;
+
+    if (paths.length > 0){
+        app.locals.activeUrl = [1];
+    }
+    else {
+        app.locals.activeUrl = 'index';
+    }
+    next();
+});
 // ejs + ejs locals
-var engine = require('ejs-locals')
+let engine = require('ejs-locals')
 // set the view engine to ejs
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
@@ -31,7 +65,7 @@ app.get('/browse', function(req, res, next) {
 // render business
 app.get('/:page?', function(req, res, next) {
   //res.sendFile(path.join(__dirname, 'public', 'business.html'))
-  var page = req.params.page || 'index';
+  let page = req.params.page || 'index';
   console.log('running ' + page );
   res.render(page);
 });
@@ -47,4 +81,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.sendFile(path.join(__dirname, 'public', '404.html'))
 });
+
 module.exports = app;
